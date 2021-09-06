0f9f2891a2085cbd8316a1f679465ce4905da77a
2Gyoon <2gyoon52@naver.com>
2021-09-06 18:12:47 +0900

    0906 변수와 상수

---
+++ main.js
@@ -0,0 +1,23 @@
+//변수와 상수 과제
+// 1번
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+// alert(admin);
+
+// 2번
+
+const ourPlanet = "Earth";
+
+const currentUserName = "Anna";
+
+// 3번
+
+const BIRTHDAY = '18 April 1982';
+
+const age = new Date(BIRTHDAY).getFullYear();
+const today = new Date().getFullYear();
+console.log(today - age);
