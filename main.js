fd8a611b74f970dd1195cd03ed66ccfd9053fc54
2Gyoon <2gyoon52@naver.com>
2021-09-06 23:56:37 +0900

    0906 자료형, 상호작용

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //변수와 상수 과제
 // 1번
 let admin;
-let name;
+// let name;
 
-name = "John";
-admin = name;
+// name = "John";
+// admin = name;
 
 // alert(admin);
 
@@ -21,3 +21,19 @@ const BIRTHDAY = '18 April 1982';
 const age = new Date(BIRTHDAY).getFullYear();
 const today = new Date().getFullYear();
 console.log(today - age);
+
+// 자료형 과제
+let name = "Ilya";
+
+// alert( `hello ${1}` ); // 답: hello 1
+
+// alert( `hello ${"name"}` ); // hello name
+
+// alert( `hello ${name}` ); // hello Ilya
+
+
+// alert, prompt, confirm을 이용한 상호작용 과제
+
+let question = prompt("당신의 이름이 무엇인가요?", "홍길동");
+
+alert(question);
